620a79a2ac0cdfc95f8ecf92c584aac67f63dec9
Kevin Bandison <kbandison@gmail.com>
2022-10-26 18:51:18 -0400

    initial commit

--- main.js
+++ main.js
@@ -1,10 +1,44 @@
-'use strict';
+// 'use strict';
 
+//Functions
 let addStrikeThrough = () => {
-
+  let item1 = document.querySelector('li');
+  item1.style.textDecoration = 'line-through';
 }
 
 let setImage = (id, url) => {
   let image = document.querySelector(`#${id}`);
-  image.source = url;
+  image.src = url;
+  image.style.height = '200px';
 }
+
+let removeLi = () => {
+  let item1 = document.querySelector('li');
+  item1.remove();
+}
+
+let fontId = (id, size) => {
+  let font = document.querySelector(`${id}`);
+  font.style.fontSize = `${size}px`;
+}
+
+let addElem = element => {
+  let argument = document.querySelector('#arguments');
+  argument.appendChild(element);
+}
+
+let newImage = document.createElement('img');
+newImage.src = 'img/Tyrian.jpg';
+newImage.style.height = '200px';
+
+/////////////////////////////////////////////////////////
+
+//Calls
+addStrikeThrough();
+setImage('image-1', 'img/Sansa.jpg');
+setImage('image-2', 'img/Jon.webp');
+setImage('image-3', 'img/Daeneerys.webp');
+removeLi();
+removeLi();
+fontId('h1', 50);
+addElem(newImage);
